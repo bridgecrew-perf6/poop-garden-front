f572309f3e34630d52705aeb596987dc046d027b
mhendricks96 <mhendricks96@yahoo.com>
2022-04-14 14:16:11 -0700

    added fab button that leads to friend requests

---
+++ src/components/Friends/pendingRequests.tsx
@@ -0,0 +1,13 @@
+
+
+
+const PendingRequests: React.FC = () => {
+
+  return (
+    <p className="ion-text-center">
+      You have no friends requests currently waiting
+    </p>
+  )
+}
+
+export default PendingRequests;
