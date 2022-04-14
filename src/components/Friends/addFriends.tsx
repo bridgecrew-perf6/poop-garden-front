3816195ccada8883fabd03eb7b28d315edb0afe2
mhendricks96 <mhendricks96@yahoo.com>
2022-04-14 13:14:06 -0700

    working on friend search feature

--- src/components/Friends/addFriends.tsx
+++ src/components/Friends/addFriends.tsx
@@ -6,38 +6,47 @@ import React, { useState, useEffect } from 'react';
 import useResourceUsers from '../../hooks/useResourceUsers'
 
 const AddFriends: React.FC = () => {
-  
-  // const friends = useStoreState(FriendStore, getFriends);
+
+  // what the user is typing in
   const [possibleFriend, setPossibleFriend] = useState<any>();
-  const [friendMatch, setFriendMatch] = useState<any>()
-
+  //original list of users
   const { resourcesUsers } = useResourceUsers();
-
+  //list of users minus friends and self
+  const [potentialFriends, setPotentialFriends] = useState<any>([])
+  //list of friends
   const friends = useStoreState(FriendStore, getFriends);
  
-  console.log(resourcesUsers);
+  // console.log(friends);
+  console.log(potentialFriends);
+
 
   useEffect(() => {
-    if (friendMatch) {
-      console.log(friends)
-      console.log('found', friendMatch.username, 'id#', friendMatch.id)
-    }
+    // filtering through users
+    if (resourcesUsers && friends) {
+      let friendIds: any[] = []
 
-  },[friendMatch, friends])
-
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    
-    for (let i = 0; i < resourcesUsers.length; i++) {
-      let user = resourcesUsers[i];
-      let userName = user.username;
-      if (userName === possibleFriend){
-        setFriendMatch(resourcesUsers[i])
+      for (let i = 0; i < friends.length; i++) {
+        let friend = friends[i];
+        let id = friend.id;
+        friendIds.push(id)
       }
+      setPotentialFriends(resourcesUsers.filter((user: any) => !friendIds.includes(user.id))); 
     }
-    // login(tempName, tempPassword)
-    // router.push('/tab1')
-  }
+  },[resourcesUsers, friends])
+
+  // const handleSubmit = (e: { preventDefault: () => void; }) => {
+  //   e.preventDefault();
+    
+  //   for (let i = 0; i < resourcesUsers.length; i++) {
+  //     let user = resourcesUsers[i];
+  //     let userName = user.username;
+  //     if (userName === possibleFriend){
+  //       setFriendMatch(resourcesUsers[i])
+  //     }
+  //   }
+  //   // login(tempName, tempPassword)
+  //   // router.push('/tab1')
+  // }
   // console.log(friends);
   
 
@@ -45,37 +54,37 @@ const AddFriends: React.FC = () => {
     <>
       <h3 className="ion-text-center">You cant just go snooping around people's crap!</h3> 
       <p className="ion-text-center">Enter the username of a friend and we'll ask them how they feel about becoming buddies</p>
-      <form onSubmit={(e) => {handleSubmit(e)}
-        }>
-          <IonItem>
-            
-            <IonSearchbar value={possibleFriend} onIonChange={e => setPossibleFriend(e.detail.value!)} showCancelButton="never"></IonSearchbar>
+      
+        <IonItem>
+          
+          <IonSearchbar value={possibleFriend} onIonChange={e => setPossibleFriend(e.detail.value!)} showCancelButton="never"></IonSearchbar>
 
-          </IonItem>
+        </IonItem>
+        <h4 className="ion-text-center">Possible Friends</h4>
 
-          <IonButton className="ion-margin-top" type="submit" expand="block">
+          {/* <IonButton className="ion-margin-top" type="submit" expand="block">
             find {possibleFriend}
-          </IonButton>
-      </form>
-      {resourcesUsers ?
+          </IonButton> */}
+      
+      {potentialFriends ?
 
       <IonList>
         {// eslint-disable-next-line array-callback-return
-        resourcesUsers.map((user: any, index: React.Key | null | undefined) => {
+        potentialFriends.map((user: any, index: React.Key | null | undefined) => {
 
           // if (`${possibleFriend}` === user.username) {
-          if (user.username.includes(`${possibleFriend}`)) {
+          if (possibleFriend && user.username.includes(`${possibleFriend}`)) {
 
-          return <IonItem key={index}>
-            <IonAvatar>
-                    <img src={`https://avatars.dicebear.com/api/bottts/${user.id}${user.poopInfo}.svg?colorful=true`} alt={'little robot avatar for each person'}/>
-            </IonAvatar>
-            <IonLabel>
-                <h1>{user.username}</h1>
-                <h3>{user.email}</h3>
-                {/* <p>{friend.poopInfo}</p> */}    
-            </IonLabel>
-          </IonItem>
+            return <IonItem key={index}>
+              <IonAvatar>
+                      <img src={`https://avatars.dicebear.com/api/bottts/${user.id}${user.poopInfo}.svg?colorful=true`} alt={'little robot avatar for each person'}/>
+              </IonAvatar>
+              <IonLabel>
+                  <h1>{user.username}</h1>
+                  <h3>{user.email}</h3>
+                  {/* <p>{friend.poopInfo}</p> */}    
+              </IonLabel>
+            </IonItem>
           }
 
         })}
