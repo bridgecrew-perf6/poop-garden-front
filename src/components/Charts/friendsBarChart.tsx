3503715519cad56d88de65700493adc825d29fc5
mhendricks96 <mhendricks96@yahoo.com>
2022-04-22 10:17:19 -0700

    took out unused variables for deployment

--- src/components/Charts/friendsBarChart.tsx
+++ src/components/Charts/friendsBarChart.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react';
-// import { RefresherEventDetail } from '@ionic/core';
 import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 //connecting to state store
-// import { FriendStore } from "../../store";
 import { FriendStore } from "../../store";
 import { UserStore } from "../../store"
-import { PoopStore } from "../../store";
-import { getPoopProfiles, getUserInfo, getFriends } from "../../store/Selectors";
+import { getUserInfo, getFriends } from "../../store/Selectors";
 import { useStoreState } from "pullstate";
 
 Chart.register(...registerables);
@@ -16,38 +12,12 @@ Chart.register(...registerables);
 
 const FriendsBarChart: React.FC = () => {
   // variables retrieved from global state
-  // const friends = useStoreState(FriendStore, getFriends);
   const userInfo = useStoreState(UserStore, getUserInfo);
-  const poopProfiles = useStoreState(PoopStore, getPoopProfiles)
   const friends = useStoreState(FriendStore, getFriends);
 
   const [tempNames, setTempNames] = useState<any>([]);
   const [tempPoop, setTempPoop] = useState<any>([]);
 
-
-  // functions to get only the data we need for the chart
-  // const getNames = (array: any[]) => {
-  //   let namesArray = [];
-  //   for (let i = 0; i < array.length; i++) {
-  //     if (array[i].poopInfo) {
-  //       let name = array[i].username || array[i].nickname;
-  //       namesArray.push(name);
-  //     }
-  //   }
-  //   namesArray.push(userInfo.username)
-  //   return namesArray;
-  // };
-
-  // const getPoop = (array: any[]) => {
-  //   let poopArray = [];
-  //   for (let i = 0; i < array.length; i++) {
-  //     if (array[i].poopInfo) {
-  //       let poop = array[i].poopInfo;
-  //       poopArray.push(poop);
-  //     }
-  //   }
-  //   return poopArray;
-  // };
 
   useEffect(() => {
     if (userInfo && friends){
