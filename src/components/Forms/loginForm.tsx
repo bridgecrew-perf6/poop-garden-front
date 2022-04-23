159d2e2243dfe1970281a7f66d1a2ed629c823fd
mhendricks96 <mhendricks96@yahoo.com>
2022-04-23 11:08:53 -0700

    added error handling to login form for when user types in wrong password

--- src/components/Forms/loginForm.tsx
+++ src/components/Forms/loginForm.tsx
@@ -4,6 +4,8 @@ import {
   IonInput,
   useIonRouter,
   IonLabel,
+  // IonContent,
+  IonToast,
 } from "@ionic/react";
 import React, { useState } from "react";
 import { useAuth } from "../../contexts/auth.js";
@@ -11,38 +13,55 @@ import { useAuth } from "../../contexts/auth.js";
 const LoginForm: React.FC = () => {
   const [tempName, setTempName] = useState<string>();
   const [tempPassword, setTempPassword] = useState<any>();
+  const [showToast, setShowToast] = useState(false);
   const router = useIonRouter();
   const { login } = useAuth();
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    login(tempName, tempPassword);
-    router.push("/tab1");
+    login(tempName, tempPassword)
+    .then(() => {
+      router.push("/tab1");
+    }).catch(() => {
+      // console.log('wrong password')
+      setShowToast(true);
+    })
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        handleSubmit(e);
-      }}
-    >
-      <IonItem>
-        <IonLabel position="floating">Name</IonLabel>
-        <IonInput onIonChange={(e) => setTempName(e.detail.value!)}></IonInput>
-      </IonItem>
+    <>
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message="Incorrect username or password"
+        duration={2000}
+        color="danger"
+        position="top"
+      />
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <IonItem>
+          <IonLabel position="floating">Name</IonLabel>
+          <IonInput onIonChange={(e) => setTempName(e.detail.value!)}></IonInput>
+        </IonItem>
 
-      <IonItem>
-        <IonLabel position="floating">Password</IonLabel>
-        <IonInput
-          type="password"
-          onIonChange={(e) => setTempPassword(e.detail.value!)}
-        ></IonInput>
-      </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput
+            type="password"
+            onIonChange={(e) => setTempPassword(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
 
-      <IonButton className="ion-margin-top" type="submit" expand="block">
-        Log In
-      </IonButton>
-    </form>
+        <IonButton className="ion-margin-top" type="submit" expand="block">
+          Log In
+        </IonButton>
+      </form>
+
+    </>
   );
 };
 
